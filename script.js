541edf44429fe3bc55bb42ebbcdce3cd0f59617e
Taleb88 <tdamaree18@gmail.com>
2020-08-18 16:32:45 -0400

    Switching on/off a light bulb

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulbOn() {
+    let x = document.getElementById('myImage');
+    x.src = 'light-on.png';
+}
+
+function bulbOff() {
+    let x = document.getElementById('myImage');
+    x.src = 'light-off.png';
+}
